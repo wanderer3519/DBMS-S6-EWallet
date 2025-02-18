bf427b00172fdd95c3773e498afba319fdbeb404
Krish_Raziel19 <112201013@smail.iitpkd.ac.in>
2025-02-18 23:51:46 +0530

    Added bootstrap, font awesome, spacelab

---
+++ dbms-frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <Button>Butt</Button>
+      <i class="fa-brands fa-react"></i>
+    </div>
+  );
+}
+
+export default App;
